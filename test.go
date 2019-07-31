b45d7a17213c90d0ceb3578f48f4a491740dd465
Leo Zhong <yunji@uber.com>
2019-07-31 12:37:09 -0700

    update

--- test.go
+++ test.go
@@ -64,3 +64,17 @@ func (f *FOO) FooMethod() {
 
 type itest interface {
 }
+
+const ColumnBranch = "branch"
+const ColumnFilePath = "file_path"
+const ColumnReportUri = "report_file_u_r_i"
+const ColumnRepo = "repository"
+const ColumnLineCovered = "lines_covered"
+const ColumnTimestamp = "timestamp"
+const ColumnSha = "sha"
+const ColumnLineMissed = "lines_missed"
+const ColumnModule = "module"
+const ColumnPackage = "package"
+const ColumnCoverageType = "coverage_type"
+const FileMetricInsertionSuccess = "file_metrics.insertion.success"
+const FileMetricInsertionFailure = "file_metrics.insertion.failure"
